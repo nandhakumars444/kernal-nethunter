dd7f13c75edd52bd76f70ce4b4220f7b87cd577f
Nandha kumar S <86781011+nandhakumars444@users.noreply.github.com>
2026-06-12 08:43:23 +0530

    Update syscalls.h

--- include/linux/syscalls.h
+++ include/linux/syscalls.h
@@ -189,6 +189,7 @@ extern struct trace_event_functions exit_syscall_print_funcs;
 #define SYSCALL_DEFINE4(name, ...) SYSCALL_DEFINEx(4, _##name, __VA_ARGS__)
 #define SYSCALL_DEFINE5(name, ...) SYSCALL_DEFINEx(5, _##name, __VA_ARGS__)
 #define SYSCALL_DEFINE6(name, ...) SYSCALL_DEFINEx(6, _##name, __VA_ARGS__)
+#define __PROTECT(...) asmlinkage_protect(__VA_ARGS__)
 
 #define __SYSCALL_DEFINEx(x, name, ...)                                        \
         asmlinkage long sys##name(__MAP(x,__SC_DECL,__VA_ARGS__));             \
@@ -203,20 +204,6 @@ extern struct trace_event_functions exit_syscall_print_funcs;
         }                                                                       \
         static inline long SYSC##name(__MAP(x,__SC_DECL,__VA_ARGS__))
 
-#define __PROTECT(...) asmlinkage_protect(__VA_ARGS__)
-#define __SYSCALL_DEFINEx(x, name, ...)					\
-	asmlinkage long sys##name(__MAP(x,__SC_DECL,__VA_ARGS__))	\
-		__attribute__((alias(__stringify(SyS##name))));		\
-	static inline long SYSC##name(__MAP(x,__SC_DECL,__VA_ARGS__));	\
-	asmlinkage long SyS##name(__MAP(x,__SC_LONG,__VA_ARGS__));	\
-	asmlinkage long SyS##name(__MAP(x,__SC_LONG,__VA_ARGS__))	\
-	{								\
-		long ret = SYSC##name(__MAP(x,__SC_CAST,__VA_ARGS__));	\
-		__MAP(x,__SC_TEST,__VA_ARGS__);				\
-		__PROTECT(x, ret,__MAP(x,__SC_ARGS,__VA_ARGS__));	\
-		return ret;						\
-	}								\
-	static inline long SYSC##name(__MAP(x,__SC_DECL,__VA_ARGS__))
 
 asmlinkage long sys32_quotactl(unsigned int cmd, const char __user *special,
 			       qid_t id, void __user *addr);
